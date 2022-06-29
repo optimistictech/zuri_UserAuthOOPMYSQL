fada4724605a0eae7053977476cf54cf66ee3c89
Samson Olamide <olaorosamson3@gmail.com>
2022-06-29 02:49:40 +0100

    All classes and methods implemented

--- classes/Dbh.php
+++ classes/Dbh.php
@@ -1,3 +1,25 @@
 <?php
 
+class Dbh {
+    public $hostname = "localhost";
+    public $username = "root";
+    public $password = "";
+    public $dbname = "zuriphp";
+
+
+    protected function connect(){
+
+        $db = new mysqli($this->hostname, $this->username, $this->password, $this->dbname);
+
+        return $db ;
+    }
+
+
+
+
+
+
+}
+
+?>
 
--- classes/Route.php
+++ classes/Route.php
@@ -1,3 +1,23 @@
 <?php
+class FormController extends UserAuth{
+
+    public $fullname;
+    public $email;
+    public $password;
+    public $confirmPassword;
+    public $country;
+    public $gender;
+
+    public function __construct()
+    {
+        $this->db = new Dbh();
+    }
+
+    public function handleForm(){
+        
+    }
 
 
+}
+
+?>
